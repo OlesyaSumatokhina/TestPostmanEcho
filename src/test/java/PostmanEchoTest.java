ebc157a242b16580db1387b08954420f18c19553
Olesya Sumatokhina <olesya_po@mail.ru>
2024-02-08 15:56:43 +0300

    changeCurrentTest

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,8 +1,11 @@
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
+    @Test
     public void shouldPostBodyTest(){
         // Given - When - Then
 // Предусловия
@@ -16,7 +19,7 @@ public class PostmanEchoTest {
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", equalTo("same dat"))
+                .body("data", equalTo("some data"))
 
         ;
     }
